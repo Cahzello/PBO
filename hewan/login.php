c528d7bd3d1f666c1751e92a8a51087adea45127
Cahzello <rizkyrasya.m@gmail.com>
2022-10-05 14:11:12 +0700

    update

---
+++ hewan/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        *, html, body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="mobil">
+    <form method="get" action="penitipan.php">
+        <table>
+            <h1>Login</h1>
+            <tr>
+                <td>Nama</td>
+                <td>: <input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td>No. Kontak</td>
+                <td>: <input type="text" name="no"></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>: <input type="text" name="alamat"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>&nbsp;&nbsp; <input type="submit" value="Login"></td>
+            </tr>
+        </table>
+        <p>Isi dengan benar!</p>
+    </form>
+    </div>
+</body>
+</html>
