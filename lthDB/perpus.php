523ecfee4973f66236c96ec7c37eec2ab113b8d2
“RizkyRasya” <rizkyrasya.m@gmail.com>
2022-10-19 15:08:23 +0700

    update

---
+++ lthDB/perpus.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'db.php';
+
+$data_anggota = $db->readAnggota();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB</title>
+</head>
+<body>
+    <h3 align="center" >Data Anggota Perpustakaan</h3>
+    <table border="1" cellpadding="5px" align="center">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+        </tr>
+        <tr>
+            
+        </tr>
+    </table>
+</body>
+</html>
