773a92e72a2175f823e5029f7de2921c2e0eb911
Rizky Rasya <rizkyrasya.m@gmail.com>
2022-10-26 21:02:47 +0700

    update

---
+++ SQL/perpus.sql
@@ -0,0 +1,17 @@
+INSERT INTO `tb_anggota` (`id_anggota`, `nama_anggota`) VALUES (NULL, 'Tae Hanazono');
+INSERT INTO `tb_anggota` (`id_anggota`, `nama_anggota`) VALUES (NULL, 'Rimi Ushigome');
+INSERT INTO `tb_anggota` (`id_anggota`, `nama_anggota`) VALUES (NULL, 'Saya Yamabuki');
+
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang`) VALUES (NULL, 'BanG Dream!: Star Beat', 'Nakamura Kou');
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang`) VALUES (NULL, 'BanG Dream!', 'Nakamura Kou');
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang`) VALUES (NULL, 'BanG Dream! Girls Band Party! Event Diary', 'Craft Egg / Bushiroad');
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang`) VALUES (NULL, 'GARUPA☆PICO Comic Anthology', 'Various Author');
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang`) VALUES (NULL, 'Roselia Stage', 'Pepako Dokuta');
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang`) VALUES (NULL, 'RAiSe! The story of my music', 'Nakamura Kou');
+
+INSERT INTO `tb_petugas_perpus` (`id_petugas`, `nama_petugas`, `username`, `password`) VALUES (NULL, 'Yukina Minato', 'lisa123', 'meow123');
+INSERT INTO `tb_petugas_perpus` (`id_petugas`, `nama_petugas`, `username`, `password`) VALUES (NULL, 'Lisa Imai', 'yukina123', 'kukitaberu');
+INSERT INTO `tb_petugas_perpus` (`id_petugas`, `nama_petugas`, `username`, `password`) VALUES (NULL, 'Sayo Hikawa', 'potato', 'renshuu');
+INSERT INTO `tb_petugas_perpus` (`id_petugas`, `nama_petugas`, `username`, `password`) VALUES (NULL, 'Rinko Shirokane', 'gamer', 'piano');
+INSERT INTO `tb_petugas_perpus` (`id_petugas`, `nama_petugas`, `username`, `password`) VALUES (NULL, 'Ako Udagawa', 'Necromancer', 'sugoii');
+
