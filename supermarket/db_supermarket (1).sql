8a35a7b8b2fd54c59a05f50e2721fe2400788c9c
“RizkyRasya” <rizkyrasya.m@gmail.com>
2022-11-09 15:00:25 +0700

    update

--- supermarket/db_supermarket (1).sql
+++ supermarket/db_supermarket (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 03, 2022 at 03:00 AM
+-- Generation Time: Nov 09, 2022 at 08:56 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -39,7 +39,32 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama_barang`, `total_barang`, `harga`) VALUES
-(10, 'Sabun', 4, 2000);
+(10, 'Sabun', 4, 2000),
+(11, 'odol', 2, 5000),
+(12, 'baju', 1, 40000),
+(13, 'celana', 2, 50000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id_pembelian` int(11) NOT NULL,
+  `tanggal_pembelian` date NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembelian`
+--
+
+INSERT INTO `pembelian` (`id_pembelian`, `tanggal_pembelian`, `id_barang`, `quantity`) VALUES
+(1, '2022-11-09', 10, 2),
+(2, '2022-11-09', 11, 1),
+(3, '2022-11-09', 12, 3);
 
 --
 -- Indexes for dumped tables
@@ -52,6 +77,13 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
+-- Indexes for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id_pembelian`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,7 +91,23 @@ ALTER TABLE `barang`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_barang` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
