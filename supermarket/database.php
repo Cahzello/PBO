97ae1b13d8941ee82bf4e8ff473f17b37cc3182f
“RizkyRasya” <rizkyrasya.m@gmail.com>
2022-11-17 07:52:55 +0700

    update

--- supermarket/database.php
+++ supermarket/database.php
@@ -1,36 +1,40 @@
 <?php
 
-class database{
+class database
+{
 
     private $dbHost = "localhost";
     private $dbUser = "root";
     private $dbPass = "";
     private $dbName = "db_supermarket";
 
-    function connectMySQL (){
+    function connectMySQL()
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
-        if (!$koneksi){
+        if (!$koneksi) {
             die("Koneksi ke DB gagal: " . mysqli_connect_error());
         } else {
             // echo "Koneksi ke DB {$this->dbName} Berhasil";
         }
     }
 
-    function readBarang(){
+    function readBarang()
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "select * from barang");
 
-        while ($row = mysqli_fetch_array($query)){
-            $data[]= $row;
+        while ($row = mysqli_fetch_array($query)) {
+            $data[] = $row;
         }
         return $data;
     }
 
-    function insertBarang($namaBarang, $totalBarang, $harga){
+    function insertBarang($namaBarang, $totalBarang, $harga)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "INSERT INTO barang (nama_barang, total_barang, harga ) VALUE ('$namaBarang', '$totalBarang', '$harga')");
 
-        if($query){
+        if ($query) {
             echo "Data berhasil ditambahkan";
             header('location:index.php');
         } else {
@@ -38,11 +42,12 @@ class database{
         }
     }
 
-    function hapusBarang($id){
+    function hapusBarang($id)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "DELETE FROM barang WHERE id_barang =" . $id . "");
 
-        if($query){
+        if ($query) {
             echo "data berhasil dihapus";
             header('location:index.php');
         } else {
@@ -50,28 +55,30 @@ class database{
         }
     }
 
-    function getBarangById($field, $id){
+    function getBarangById($field, $id)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
-        $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE id_barang =" . $id ."");
+        $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE id_barang =" . $id . "");
         $data = mysqli_fetch_array($query);
 
 
-        if($field == 'id_barang'){
+        if ($field == 'id_barang') {
             return $data['id_barang'];
         } else if ($field == 'nama_barang') {
             return $data['nama_barang'];
-        } else if ($field == 'jumlah_barang'){
+        } else if ($field == 'jumlah_barang') {
             return $data['total_barang'];
-        } else if ($field == 'harga_barang'){
+        } else if ($field == 'harga_barang') {
             return $data['harga'];
         }
     }
 
-    function updateBarang($id, $namaBarang, $totalBarang, $harga){
+    function updateBarang($id, $namaBarang, $totalBarang, $harga)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "UPDATE barang SET nama_barang = '$namaBarang', total_barang = '$totalBarang', harga = '$harga' WHERE id_barang = $id;");
 
-        if($query){
+        if ($query) {
             echo "data berhasil dihapus";
             header('location:index.php');
         } else {
@@ -79,21 +86,23 @@ class database{
         }
     }
 
-    function readPembelian(){
+    function readPembelian()
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "SELECT * from pembelian JOIN barang ON pembelian.id_barang = barang.id_barang");
 
-        while ($row = mysqli_fetch_array($query)){
-            $data[]= $row;
+        while ($row = mysqli_fetch_array($query)) {
+            $data[] = $row;
         }
         return $data;
     }
 
-    function insertPembelian($tanggal, $kuantitas, $barang){
+    function insertPembelian($tanggal, $kuantitas, $barang)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "INSERT INTO pembelian (tanggal_pembelian, quantity, id_barang) VALUE ('$tanggal', '$kuantitas', '$barang')");
 
-        if($query){
+        if ($query) {
             echo "Data berhasil ditambahkan";
             header('location:index.php');
         } else {
@@ -102,11 +111,12 @@ class database{
         }
     }
 
-    function hapusPembelian($id){
+    function hapusPembelian($id)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "DELETE FROM pembelian WHERE id_pembelian =" . $id . "");
 
-        if($query){
+        if ($query) {
             echo "data berhasil dihapus";
             header('location:index.php');
         } else {
@@ -114,11 +124,12 @@ class database{
         }
     }
 
-    function updatePembelian($id, $tanggal, $barang, $qt){
+    function updatePembelian($id, $tanggal, $barang, $qt)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
         $query = mysqli_query($koneksi, "UPDATE pembelian SET tanggal_pembelian = '$tanggal', id_barang = '$barang', quantity = '$qt' WHERE id_pembelian = $id;");
 
-        if($query){
+        if ($query) {
             echo "data berhasil dihapus";
             header('location:index.php');
         } else {
@@ -127,19 +138,20 @@ class database{
         }
     }
 
-    function getPembelianById($field, $id){
+    function getPembelianById($field, $id)
+    {
         $koneksi = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
-        $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE id_pembelian =" . $id ."");
+        $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE id_pembelian =" . $id . "");
         $data = mysqli_fetch_array($query);
 
 
-        if($field == 'tanggal'){
+        if ($field == 'tanggal') {
             return $data['tanggal_pembelian'];
         } else if ($field == 'namaBarang') {
             return $data['nama_barang'];
-        } else if ($field == 'quantity'){
+        } else if ($field == 'quantity') {
             return $data['quantity'];
-        } 
+        }
     }
 }
 
@@ -150,50 +162,47 @@ $db->readPembelian();
 $db->readBarang();
 // $db->insertAnggota($nama);
 
-if(isset($_POST['submitBarang'])){
+if (isset($_POST['submitBarang'])) {
     $namaBarang = $_POST['nama_barang'];
     $totalBarang = $_POST['total_barang'];
     $harga = $_POST['harga'];
     $db->insertBarang($namaBarang, $totalBarang, $harga);
-} else if (isset($_POST['prosesUpdateBarang'])){
+} else if (isset($_POST['prosesUpdateBarang'])) {
     $id = $_POST['id_barang'];
     $barang = $_POST['nama_barang'];
     $jumlah = $_POST['jumlah_barang'];
     $harga = $_POST['harga_barang'];
     $db->updateBarang($id, $barang, $jumlah, $harga);
-} else if (isset($_POST['submitPembelian'])){
+} else if (isset($_POST['submitPembelian'])) {
     $tanggal = $_POST['tanggal'];
     $kuantitas = $_POST['kuantitas'];
     $barang = $_POST['barang'];
     $db->insertPembelian($tanggal, $kuantitas, $barang);
-} else if (isset($_POST['prosesUpdatePembelian'])){
+} else if (isset($_POST['prosesUpdatePembelian'])) {
     $id = $_POST['id_pembelian'];
     $tanggal = $_POST['tanggal'];
     $barang = $_POST['namaBarang'];
     $qt = $_POST['quantity'];
-    $db->updatePembelian($id ,$tanggal, $barang, $qt);
+    $db->updatePembelian($id, $tanggal, $barang, $qt);
 }
 
-if(isset($_GET['aksi'])){
-   if($_GET['aksi'] == 'hapusBarang'){
-        $id=$_GET['id'];
+if (isset($_GET['aksi'])) {
+    if ($_GET['aksi'] == 'hapusBarang') {
+        $id = $_GET['id'];
         $db->hapusBarang($id);
-   }
-
-}
-
-if(isset($_GET['aksi'])){
-    if($_GET['aksi'] == 'hapusPembelian'){
-         $id=$_GET['id'];
-         $db->hapusPembelian($id);
-    }
- 
- } 
-
-if(isset($_GET['aksi'])){
-    if(isset($_GET['aksi']) == 'update'){
-
-        $id=$_GET['id'];
     }
 }
 
+if (isset($_GET['aksi'])) {
+    if ($_GET['aksi'] == 'hapusPembelian') {
+        $id = $_GET['id'];
+        $db->hapusPembelian($id);
+    }
+}
+
+if (isset($_GET['aksi'])) {
+    if (isset($_GET['aksi']) == 'update') {
+
+        $id = $_GET['id'];
+    }
+}
